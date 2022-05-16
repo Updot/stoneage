8ef84caa4e8a878fee55849c9924653a0306abf5
sourab <shubpramanik@gmail.com>
2022-05-16 15:35:46 +0530

    UI Improvements

--- scripts/index.js
+++ scripts/index.js
@@ -82,6 +82,11 @@ var swiper = new Swiper(".mySwiper", {
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
+  initialSlide: 1,
+  navigation: {
+    nextEl: ".slider_nav_next-btn",
+    prevEl: ".slider_nav_prev-btn",
+  },
   coverflowEffect: {
     rotate: 0,
     stretch: 0,
