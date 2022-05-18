3eee93f60c83d9c711058bd2f81a077c0ccbe54a
sourab <shubpramanik@gmail.com>
2022-05-18 13:16:43 +0530

    Links added

--- scripts/article.js
+++ scripts/article.js
@@ -1,90 +1,12 @@
-const body = document.querySelector("body");
-const footer_logo = document.querySelector(".footer_logo");
-const sub_nav_btn = document.querySelector(".sub_nav_btn");
-const sub_nav = document.querySelector(".sub_nav");
-const theme_toggle = document.querySelector(".theme_toggle");
-const search_bar = document.querySelector(".search_bar");
-const nav_search_btn = document.querySelector(".nav_search_btn");
 const commentScrollArea = document.querySelector(".comment-scroll-area");
 const other_articles_scrollarea = document.querySelector(
   ".other_articles-scroll-area"
 );
 const share_options = document.querySelector(".share_options-wrap");
-// theme fetch
-window.onload = () => {
-  const themeMode = localStorage.getItem("theme");
-  themeMode === "dark"
-    ? body.classList.add("dark")
-    : body.classList.remove("dark");
-  handleLogoChange();
-  handleThemeIconChange();
-  handleCommentFetch();
-  handleArticlesFetch();
-};
-
-//theme toggle
-const handleThemeToggle = () => {
-  body.classList.toggle("dark");
-  body.classList.value === "dark"
-    ? localStorage.setItem("theme", "dark")
-    : localStorage.setItem("theme", "light");
-  handleLogoChange();
-  handleThemeIconChange();
-};
-
-const handleLogoChange = () => {
-  body.classList.value === "dark"
-    ? (footer_logo.src = "./assets/logo-big_dark.svg")
-    : (footer_logo.src = "./assets/logo-big_light.svg");
-};
-
-const handleThemeIconChange = () => {
-  body.classList.value === "dark"
-    ? (theme_toggle.src = "./assets/dark-icon.svg")
-    : (theme_toggle.src = "./assets/light_icon.svg");
-};
-
-// sub nav toggle
-const handleSubNavToggle = () => {
-  sub_nav_btn.classList.toggle("active");
-  sub_nav.classList.toggle("active");
-};
-
-function closeSubNav() {
-  sub_nav.classList.remove("active");
-  sub_nav_btn.classList.remove("active");
-}
-document.addEventListener("click", (e) => {
-  !sub_nav_btn.contains(e.target) &&
-    sub_nav.classList.contains("active") &&
-    closeSubNav();
-});
-
-// search
-const handleSearchBarToggle = () => {
-  search_bar.classList.toggle("active");
-  nav_search_btn.classList.toggle("active");
-};
-
-function closeSearch() {
-  nav_search_btn.classList.remove("active");
-  search_bar.classList.remove("active");
-}
-
-document.addEventListener("click", (e) => {
-  if (
-    !nav_search_btn.contains(e.target) &&
-    search_bar.classList.contains("active") &&
-    !search_bar.contains(e.target)
-  ) {
-    closeSearch();
-  }
-});
 
 //SHARE
 
 const handleShareBtn = () => {
-  console.log("Dsadsad");
   share_options.classList.toggle("active");
 };
 
@@ -143,3 +65,6 @@ const handleArticlesFetch = async () => {
     other_articles_scrollarea.appendChild(articleItem);
   });
 };
+
+handleCommentFetch();
+handleArticlesFetch();
